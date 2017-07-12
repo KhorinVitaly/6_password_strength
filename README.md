88116e40d738c900cd8b2dfa07a7f5cfb4b17f3b
Vitaly Khorin <khorin.v@gmail.com>
2017-07-12 22:58:50 +0600

    Реализовал задачу, заполнил описание

--- README.md
+++ README.md
@@ -1,6 +1,54 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+This script evaluate password with some criterions such as:
+* use of both upper-case and lower-case letters; 
+* inclusion numerical digits;
+* inclusion of special characters;
+* password length;
+* unique letters quantity;
+* duplicates;
+* and words from password blacklist as option. 
+
+# Quickstart
+
+You will need python 3.5 interpreter. Then you can launch 
+script in terminal or import functions in your code. 
+
+launch on Linux, Python 3.5:
+
+```#!bash
+
+$ python3 password_strength.py [<path to blacklist file for check>]
+
+#input output example
+Input your password: dbfblllfv;4
+Password rating - 6
+1 - very weak; 10 - cool
+
+Input your password: Password 
+Password rating - 1
+1 - very weak; 10 - cool
+```
+
+Example of blacklist file, use simple text file, one word in one line:
+```
+123456
+admin
+password
+test
+123
+123456789
+12345678
+```
+ 
+Example of using functions in code:
+
+```
+import password_strength
+
+rating = get_password_strength(password)
+
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,71 @@
+from sys import argv
 
 
-def get_password_strength(password):
-    pass
+def get_password_strength(password, blacklist_path=None):
+    points = 1
+    if len(password) > 3:
+        points += len(password) // 5
+        points += check_for_standard_recommendations(password)
+        points += count_unique_letters(password) // 2
+        points -= check_for_duplicate_letters(password)
+        if blacklist_path:
+            points -= check_by_blacklist(password, blacklist_path)
+
+    if points < 0:
+        points = 1
+    elif points > 10:
+        points = 10
+
+    return points
+
+
+def check_for_duplicate_letters(password):
+    for number, letter in enumerate(password):
+        if letter == password[number - 1] and letter == password[number - 2]:
+            return 2
+    return 0
+
+
+def count_unique_letters(password):
+    unique_letters = ""
+    for letter in password:
+        if letter not in unique_letters:
+            unique_letters += letter
+    return len(unique_letters)
+
+
+def check_for_standard_recommendations(password):
+    points = 0
+    control_list = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    "~`!@#$%^&*()-_+=", "1234567890"]
+    for control_string in control_list:
+        for letter in password:
+            if letter in control_string:
+                points += 1
+                break
+    return points
+
+
+def check_by_blacklist(password, blacklist_path):
+    try:
+        with open(blacklist_path, "r") as blacklist_file:
+            password_blacklist = blacklist_file.readlines()
+    except:
+        return 0
+
+    for word in password_blacklist:
+        if password in word:
+            return 8
+    return 0
 
 
 if __name__ == '__main__':
-    pass
+    password = input("Input your password: ")
+    if not password:
+        exit()
+    if len(argv) == 2:
+        rating = get_password_strength(password, argv[1])
+    else:
+        rating = get_password_strength(password)
+    print("Password rating - " + str(rating))
+    print("1 - very weak; 10 - cool")
