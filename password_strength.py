939f7429ba812052be62a952091440f6026d9102
KhorinVitaly <khorin.v@gmail.com>
2017-07-14 15:24:04 +0600

    Учел комментарии Ильи

--- password_strength.py
+++ password_strength.py
@@ -1,66 +1,73 @@
 from sys import argv
+import string, re, getpass
 
 
 def get_password_strength(password, blacklist_path=None):
     points = 1
-    if len(password) > 3:
-        points += len(password) // 5
-        points += check_for_standard_recommendations(password)
-        points += count_unique_letters(password) // 2
-        points -= check_for_duplicate_letters(password)
-        if blacklist_path:
-            points -= check_by_blacklist(password, blacklist_path)
 
-    if points < 0:
-        points = 1
-    elif points > 10:
-        points = 10
+    lenght_coef = 5
+    unique_letters_quantity_coef = 2
+    minimal_lenght = 3
+
+    min_point = 1
+    max_point = 10
+
+    if len(password) > minimal_lenght:
+        points += len(password) // lenght_coef
+        points += check_for_standard_recommendations(password)
+        points += count_unique_letters(password) // unique_letters_quantity_coef
+        points -= check_for_duplicate_letters(password)
+        try:
+            blacklist = get_blacklist(blacklist_path)
+            points -= check_by_blacklist(password, blacklist)
+        except:
+            pass
+
+    if points < min_point:
+        points = min_point
+    elif points > max_point:
+        points = max_point
 
     return points
 
 
 def check_for_duplicate_letters(password):
-    for number, letter in enumerate(password):
-        if letter == password[number - 1] and letter == password[number - 2]:
-            return 2
-    return 0
+    if re.search(r'(.)\1{1,}', password):
+        return 2
+    else:
+        return 0
 
 
 def count_unique_letters(password):
-    unique_letters = ""
-    for letter in password:
-        if letter not in unique_letters:
-            unique_letters += letter
+    unique_letters = set(password)
     return len(unique_letters)
 
 
 def check_for_standard_recommendations(password):
     points = 0
-    control_list = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                    "~`!@#$%^&*()-_+=", "1234567890"]
+    password_as_set = set(password)
+    control_list = [string.ascii_lowercase, string.ascii_uppercase,
+                    string.punctuation, string.digits]
     for control_string in control_list:
-        for letter in password:
-            if letter in control_string:
-                points += 1
-                break
+        if password_as_set & set(control_string):
+            points += 1
     return points
 
 
-def check_by_blacklist(password, blacklist_path):
-    try:
-        with open(blacklist_path, "r") as blacklist_file:
-            password_blacklist = blacklist_file.readlines()
-    except:
-        return 0
+def get_blacklist(blacklist_path):
+    with open(blacklist_path, "r") as blacklist_file:
+        return blacklist_file.readlines()
 
-    for word in password_blacklist:
+
+def check_by_blacklist(password, blacklist):
+    for word in blacklist:
         if password in word:
             return 8
     return 0
 
 
 if __name__ == '__main__':
-    password = input("Input your password: ")
+    password = getpass.unix_getpass("Input your password: ")
     if not password:
         exit()
     if len(argv) == 2:
